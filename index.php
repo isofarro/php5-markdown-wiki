b843de897b3fc3c952e9ff6ffc67da48232235c1
Isofarro <isofarro2@gmail.com>
2009-05-19 20:37:49 +0100

    Simple views for display and edit

--- index.php
+++ index.php
@@ -11,29 +11,56 @@ require_once $baseDir . '/Markdown.php';
 
 // Parsing the request
 $request = getRequest($docIndex);
-$filename = "{$docDir}{$request->page}.markdown";
+$request->filename = "{$docDir}{$request->page}.markdown";
 
-if (file_exists($filename)) {
-	echo "Found: {$filename}\n";
-	$request->content = file_get_contents($filename);
+if (file_exists($request->filename)) {
+	//echo "Found: {$request->filename}\n";
+	$request->content = file_get_contents($request->filename);
 }
 
-// If content doesn't exist go into editing mode
+// If content doesn't exist go into editing mode.
 if (is_null($request->content)) {
 	$request->action = 'edit';
 }
 
+switch($request->action) {
+	case 'display':
+		doDisplay($request);
+		break;
+	case 'edit':
+		doEdit($request);
+		break;
+	case 'history':
+	case 'admin':
+	case 'browse':
+	default:
+		echo "<h3>Action: {$request->action}</h3>";
+		break;
+}
 
 echo '<pre>'; print_r($request); echo '</pre>';
 //phpinfo();
+
+function doDisplay($request) {
+	$content = htmlspecialchars($request->content);
+	echo <<<HTML
+<pre>{$content}</pre>
+HTML;
+}
+
+function doEdit($request) {
+	echo <<<HTML
+<textarea cols="78" rows="20">{$request->content}</textarea>
+HTML;
+}
 
 
 function getRequest($docIndex) {
 	$request = (object) NULL;
 	
 	if (!empty($_REQUEST['id'])) {
-		$request->action = $_REQUEST['action'];
 		$request->page   = $_REQUEST['id'];
+		$request->action = $_REQUEST['action'];
 	} elseif (!empty($_SERVER['PATH_INFO'])) {
 		$request->page   = substr($_SERVER['PATH_INFO'], 1);
 		$request->action = 'display';
@@ -50,7 +77,8 @@ function getRequest($docIndex) {
 		$request->page .= $docIndex;
 	}
 
-	$request->content = NULL;
+	$request->filename = NULL;
+	$request->content  = NULL;
 
 	return $request;
 }
