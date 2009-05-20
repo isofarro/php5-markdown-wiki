60b9ede88363d6499b679059635a7e63b0cbe7b4
Isofarro <isofarro2@gmail.com>
2009-05-20 08:13:12 +0100

    Simple editing/save screenflow

--- index.php
+++ index.php
@@ -10,11 +10,12 @@ require_once $baseDir . '/Markdown.php';
 
 // Parsing the request
 $request = getRequest($docIndex);
-$request->filename = "{$docDir}{$request->page}.markdown";
+$request->filename = "{$docDir}{$request->page}.text";
 
 if (file_exists($request->filename)) {
 	//echo "Found: {$request->filename}\n";
 	$request->content = file_get_contents($request->filename);
+	$request->updated = filectime($request->filename);
 }
 
 // If content doesn't exist go into editing mode.
@@ -29,6 +30,9 @@ switch($request->action) {
 	case 'edit':
 		$response = doEdit($request);
 		break;
+	case 'save':
+		$response = doSave($request);
+		break;
 	case 'history':
 	case 'admin':
 	case 'browse':
@@ -40,7 +44,7 @@ switch($request->action) {
 renderPage($response);
 
 
-//echo '<pre>'; print_r($request); echo '</pre>';
+echo '<pre>'; print_r($request); echo '</pre>';
 //phpinfo();
 
 function doDisplay($request) {
@@ -52,6 +56,12 @@ function doDisplay($request) {
 <pre>{$content}</pre>
 HTML;
 
+	$response['footer'] = <<<HTML
+<ul>
+	<li><a href="{$request->path}?action=edit&amp;id={$request->page}">Edit</a></li>
+</ul>
+HTML;
+
 	return $response;
 }
 
@@ -60,10 +70,29 @@ function doEdit($request) {
 
 	$response['title']   = "Editing: {$request->page}";
 	$response['content'] = <<<HTML
-<textarea cols="78" rows="20">{$request->content}</textarea>
+<form action="{$request->path}/{$request->page}" method="post">
+	<fieldset>
+		<legend>Editing</legend>
+		<label for="title">Title:</label><br>	
+		<input type="text" name="title" id="title" size="78"><br>
+		
+		<label for="text">Content:</label><br>	
+		<textarea cols="78" rows="20" name="text" id="text">{$request->content}</textarea>
+		<br>
+
+		<input type="submit" name="preview" value="Preview">
+		<input type="submit" name="save" value="Save">
+		<input type="hidden" name="updated" value="{$request->updated}">
+	</fieldset>
+</form>
 HTML;
+	$response['footer'] = '';
 
 	return $response;
+}
+
+function doSave($request) {
+	return doDisplay($request);
 }
 
 
@@ -78,7 +107,7 @@ function renderPage($response) {
 		<div id="head"></div>
 		<div id="content">{$response['content']}</div>	
 		<div id="related"></div>	
-		<div id="foot"></div>	
+		<div id="foot">{$response['footer']}</div>	
 	</div>
 </body>
 </html>
@@ -90,20 +119,40 @@ PAGE;
 
 function getRequest($docIndex) {
 	$request = (object) NULL;
-	
-	if (!empty($_REQUEST['id'])) {
-		$request->page   = $_REQUEST['id'];
-		$request->action = $_REQUEST['action'];
-	} elseif (!empty($_SERVER['PATH_INFO'])) {
+	$request->action = 'display';
+
+	if (!empty($_SERVER['PATH_INFO'])) {
 		$request->page   = substr($_SERVER['PATH_INFO'], 1);
-		$request->action = 'display';
-	} else {
-		$request->page   = '';
-		$request->action = 'display';
 	}
+
+	if ($_SERVER['REQUEST_METHOD']=='POST') {
+		$request->post = (object) NULL;
+		$request->action = 'preview';
+
+		print_r($_POST);
+		if (!empty($_POST['save'])) {
+			$request->action = 'save';
+		}
+		
+		$request->post->title   = $_POST['title'];
+		$request->post->text    = $_POST['text'];
+		$request->post->updated = $_POST['updated'];
+	} elseif ($_SERVER['REQUEST_METHOD']=='GET') {
 	
-	if ($request->page=='') {
-		$request->page = $docIndex;
+		if (!empty($_GET['id'])) {
+			$request->page   = $_GET['id'];
+			$request->action = $_GET['action'];
+		} elseif (!empty($_SERVER['PATH_INFO'])) {
+			$request->page   = substr($_SERVER['PATH_INFO'], 1);
+			$request->action = 'display';
+		} else {
+			$request->page   = '';
+			$request->action = 'display';
+		}
+	
+		if ($request->page=='') {
+			$request->page = $docIndex;
+		}
 	}
 	
 	if ($request->page[strlen($request->page)-1]=='/') {
@@ -112,6 +161,7 @@ function getRequest($docIndex) {
 
 	$request->filename = NULL;
 	$request->content  = NULL;
+	$request->path     = '/markdown.php';
 
 	return $request;
 }
