7c7279ff95e19750882c10f3c5d53076b2c186d9
Isofarro <isofarro2@gmail.com>
2009-06-01 18:12:41 +0100

    clean up filenames before major refactoring

---
+++ index-sample.php
@@ -0,0 +1,15 @@
+<?php
+
+$appRoot = '/home/user/projects/php5-markdown/';
+
+$config = array(
+	# page storage root
+	'' => $appRoot . 'pages/';
+
+);
+
+
+
+require_once $appRoot . 'markdown-wiki.php';
+
+?>
