8247b905d3df6d7840f6dfa2bb58b1a1883ab880
Isofarro <isofarro2@gmail.com>
2009-06-01 18:08:35 +0100

    rename index.php to markdown-wiki.php for consistency

---
+++ web-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+# This file belongs within the document root of your site.
+# Change the next line to point to the index.php file inside the
+# php5-markdown-wiki directory.
+
+require_once 'php5-markdown-wiki/index.php';
+
+?>
